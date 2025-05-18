e46055a3fd2a2f102bda1d6da9050cb27494c177
Meyta Zaskiya <151342867+meytazaskiya@users.noreply.github.com>
2025-05-18 22:56:16 +0700

    Update login.php

--- webprak11/form login/login.php
+++ webprak11/form login/login.php
@@ -1,17 +1,20 @@
 <?php
 session_start();
 include 'koneksi/db.php';
-$username=$_POST['username'];
-$password=$_POST['password'];
-$query="SELECT * FROM users WHERE username='$username'";
-$result=mysqli_query($conn,$query);
-$user=mysqli_fetch_assoc($result);
-if($user && password_verify($password,$user['password'])){
-    $_SESSION['user']=$user['username'];
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$query = mysqli_query($conn, "SELECT * FROM user WHERE username='$username'");
+$data = mysqli_fetch_assoc($query);
+
+if ($data && password_verify($password, $data['password'])) {
+    $_SESSION['login'] = true;
+    $_SESSION['username'] = $username;
     header("Location: dashboard.php");
-    exit;
-}else{
-    $_SESSION['error']="Login gagal. Username atau password salah.";
-    header("Location: index.php");
-    exit; 
+    exit();
+} else {
+    header("Location: index.php?error=1");
+    exit();
 }
+?>
